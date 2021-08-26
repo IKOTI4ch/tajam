ded61bc7924df0e76c482844cf11d88a1c1a1a3e
IKOTI4ch <79322867+IKOTI4ch@users.noreply.github.com>
2021-08-26 16:00:59 +0500

    Second version
    adaptive version

--- src/js/main.js
+++ src/js/main.js
@@ -5,32 +5,94 @@ $(document).ready(function () {
         arrows: false,
         autoplay: true,
         dots: true,
-
     });
 
     $('.sayTextSlider').slick({
         autoplay: true,
         arrows: false,
         centerMode: true,
+        centerPadding: '0',
         asNavFor: '.sayImgSlider',
     });
 
     $('.sayImgSlider').slick({
         autoplay: true,
         slidesToShow: 5,
-        centerMode: true,
         slidesToScroll: 1,
         swipe: false,
         variableWidth: true,
+        centerMode: true,
+        responsive: [
+            {
+                breakpoint: 1401,
+                settings: {
+                    slidesToShow: 3,
+                    variableWidth: false,
+                    centerPadding: '0',
+                }
+            },
+
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1,
+                    variableWidth: false,
+                    centerPadding: '0',
+                }
+            },
+        ],
         asNavFor: '.sayTextSlider',
     });
 
-    $('.mainVideoButton').click(function () {
-        let a = $('.videoCheck').hasClass('autoplay');
-        if (!a == true) {
-            $('.videoCheck').attr('autoplay');
-        } else {
-            $('.videoCheck').removeAttr('autoplay');
+    $('.expertiseSlider').slick({
+        slidesToShow: 1,
+        autoplay: true,
+        centerMode: true,
+        centerPadding: '0',
+    });
+
+    $('.teamSlider').slick({
+        slidesToShow: 1,
+        autoplay: true,
+        centerMode: true,
+        centerPadding: '0',
+    });
+
+    $('.workSlider').slick({
+        slidesToShow: 1,
+        autoplay: true,
+        centerMode: true,
+        centerPadding: '0',
+    });
+
+    $('.clientSlider').slick({
+        slidesToShow: 1,
+        autoplay: true,
+        centerMode: true,
+        centerPadding: '0',
+        adaptiveHeight: true
+    });
+
+    // $('.mainVideoButton').click(function () {
+    // let a = $('.videoCheck').hasClass('autoplay');
+    // if (!a == true) {
+    //   $('.videoCheck').attr('autoplay');
+    // } else {
+    //    $('.videoCheck').removeAttr('autoplay');
+    // }
+    //});
+
+    $('.popupWork').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        mainClass: 'mfp-img-mobile',
+        image: {
+            verticalFit: true
         }
+    });
+
+    $('.myDrop').click(function () {
+        $('header').toggleClass('done');
+        $('.myDrop').toggleClass('done');
     });
 });
